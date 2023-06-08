2a1bff6bbcc5fc9e49a8af133059d9b15ea19e69
Graham Spencer <gspencer447@yahoo.com>
2023-06-08 10:01:14 -0500

    added basic structure with one component

---
+++ components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar(){
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
